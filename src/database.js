855c9f5d83dc0e640e147b65009d8c86434d764e
GuilleGold <chinaski2025@gmail.com>
2022-12-01 08:13:38 -0600

    mi sexto commit

---
+++ src/database.js
@@ -0,0 +1,15 @@
+/* 
+es para tener una conexion a una base de datos 
+y este archivo sea utilizado por 
+el archivo index.js
+
+
+moverse dentro de una carpeta 
+cd "nombre de carpeta"
+
+crear carpetas 
+
+mkdir
+en la terminar 
+mkdir config helpers models public routes view
+*/
